e250b73aa0c35d78d9dee68b0d82cbc6b7a20a2c
arthurz <Zachary.Arthur@lightsource.ca>
2022-01-12 12:43:01 -0600

    Updates Jan 2021

--- spec/connection.py
+++ spec/connection.py
@@ -11,6 +11,20 @@ import logging
 import time
 import weakref
 
+try:
+    import sgmdata
+    import os
+    from bokeh.io import output_notebook
+    os.environ['JHUB_ADMIN'] = '1'
+    os.environ['JUPYTERHUB_USER'] = 'arthurz'
+    os.environ['DB_ENV_SECRET'] = config.get('db')
+    PLOT_ENABLED = True
+except:
+    PLOT_ENABLED = False
+    print("Running without plotting enabled")
+    
+
+
 logger = logging.getLogger("SpecClient")
 logger.setLevel(logging.DEBUG)
 
@@ -56,6 +70,7 @@ class SpecChannel:
         self.isdisconnected = True
         self.registered = False
         self.value = None
+ 
 
         if not connection.on_con_lost.result():
             self.connected()
@@ -121,9 +136,9 @@ class SpecChannel:
                 if not deleted:
                     if self.access2 is None:
                         if (
-                                force
-                                or self.value is None
-                                or self.value != channelValue[self.access1]
+                            force
+                            or self.value is None
+                            or self.value != channelValue[self.access1]
                         ):
                             if isinstance(channelValue[self.access1], dict):
                                 self.value = channelValue[self.access1].copy()
@@ -133,10 +148,10 @@ class SpecChannel:
                         if self.access2 in channelValue[self.access1]:
                             if not deleted:
                                 if (
-                                        force
-                                        or self.value is None
-                                        or self.value
-                                        != channelValue[self.access1][self.access2]
+                                    force
+                                    or self.value is None
+                                    or self.value
+                                    != channelValue[self.access1][self.access2]
                                 ):
                                     self.value = self._coerce(
                                         channelValue[self.access1][self.access2]
@@ -144,6 +159,7 @@ class SpecChannel:
                     if asyncio.iscoroutinefunction(self.callback):
                         self.loop.create_task(self.callback(self.name, channelValue, epoch=epoch))
                         data_name = "_".join(str(self.name).split('/'))
+
 
                     self.isUpdated.set()
 
@@ -182,7 +198,7 @@ class SpecChannel:
                         else:
                             self.value[k1] = v1
                 if asyncio.iscoroutinefunction(self.callback):
-                    self.loop.create_task(self.callback(self.name, channelValue, epoch=epoch))
+                    self.loop.create_task(self.callback(self.name, channelValue, epoch = epoch))
 
                 self.isUpdated.set()
 
@@ -192,9 +208,10 @@ class SpecChannel:
             else:
                 self.value = channelValue
                 if asyncio.iscoroutinefunction(self.callback):
-                    self.loop.create_task(self.callback(self.name, channelValue, epoch=epoch))
+                    self.loop.create_task(self.callback(self.name, channelValue, epoch = epoch))
 
                 self.isUpdated.set()
+
 
     def read(self, timeout=3, force_read=False):
         """Read the channel value
@@ -237,6 +254,7 @@ class SpecChannel:
             connection.send_msg_chan_send(self.spec_chan_name, value, wait)
 
 
+
 class SpecProtocol(asyncio.Protocol):
     def __init__(self, loop):
         self.transport = None
@@ -247,7 +265,7 @@ class SpecProtocol(asyncio.Protocol):
         self.state = None
         self.loop = loop
         self.serverVersionF = loop.create_future()
-        self.serverVersion = 4
+        self.serverVersion= 4
         self.name = 'SPEC'
         self.nameF = loop.create_future()
         self.socket_write_event = asyncio.Event()
@@ -256,12 +274,13 @@ class SpecProtocol(asyncio.Protocol):
         self._completed_writing_event = asyncio.Event()
         self.channel_name = None
 
+
     def connection_made(self, transport):
         self.state = WAITINGFORHELLO
         self.transport = transport
         self.send_msg_hello()
 
-    async def readFromStream(self, data):
+    async def readFromStream(self,data):
         message = None
         self.buffer.append(data)
 
@@ -300,7 +319,8 @@ class SpecProtocol(asyncio.Protocol):
                                     )
                                 else:
                                     del self.registeredReplies[replyID]
-                                    reply.update(
+                                    
+                                reply.update(
                                         message.data, message.type == ERROR, message.err
                                     )
                         elif message.cmd == EVENT:
@@ -472,6 +492,7 @@ class SpecProtocol(asyncio.Protocol):
         """
         chanName = str(chanName)
 
+
         try:
             if chanName not in self.registeredChannels:
                 channel = SpecChannel(self, chanName, register)
@@ -494,6 +515,7 @@ class SpecProtocol(asyncio.Protocol):
                 "Uncaught exception in SpecConnection.registerChannel"
             )
 
+
     def unregisterChannel(self, chanName):
         """Unregister a channel
         Arguments:
@@ -511,65 +533,83 @@ class SpecClient:
         self.channel_name = None
         self.loop = asyncio.get_event_loop()
         self.transport, self.protocol = self.loop.run_until_complete(
-            self.loop.create_connection(lambda: SpecProtocol(self.loop), host, port))
+        self.loop.create_connection(lambda: SpecProtocol(self.loop), host, port))
         self.total_time = None
         self.send_command("p \"SpecClient %s, Connected\"" % config.get('version'))
+        self.sample_name = ""
 
     def register_channel(self, channel, reciever=None):
         self.protocol.registerChannel(channel, register=True, recieverSlot=reciever)
 
     def send_command(self, cmd):
         self.protocol.send_msg_cmd(cmd)
-
+        
     def setuser(self, username):
+        self.user = username
         self.send_command("SPEC_USER = \"%s\"" % username)
         self.send_command("SPEC_USERCRED[0] = \"user=%s\"" % username)
         self.send_command("SPEC_USERCRED[1] = \"secret=%s\"" % config.get('api_key'))
         self.send_command("SPEC_USER_FLAG[0] = 1")
-
+    
     def proposal(self, proposal):
         self.send_command("SPEC_PROPOSAL = \"%s\"" % proposal)
         self.send_command("SPEC_USERCRED[2] = \"proposal=%s\"" % proposal)
 
+        
     def sample(self, name, kind):
+        self.sample_name = name
         self.send_command("sample \"%s\" %d" % (name, kind))
-
+        
     def status(self):
         self.send_command("status")
-
+        
     def vent(self):
         self.send_command("vent")
-
+        
     def pump_down(self):
         self.send_command("pump_down")
-
+        
     def en(self, energy):
         self.send_command("umv en %d" % energy)
-
-    def map_holder(self):
-        self.send_command("cmesh xp -8 8 4 yp -4.5 5 25")
-
+        
+    def map_holder(self): 
+        self.send_command("cmesh xp -8 8 4 yp -5 5 25")
+        
+    def plot_holder(self, name=False):
+        if PLOT_ENABLED:
+            output_notebook()
+            if name:
+                sgmq = sgmdata.SGMQuery(sample=name, user=self.user, data=False)
+            else:
+                sgmq = sgmdata.SGMQuery(sample=self.sample_name, user=self.user, data=False)
+            data = sgmdata.SGMData(
+                        sorted([p.replace('/home/jovyan/data', '/SpecData') for p in sgmq.paths], 
+                       key=lambda x: x.split('/')[-1].split('.')[0])[-1])
+            scan = data.scans[[k for k in data.scans.keys()][0]]
+            scan[[k for k in scan.__dict__.keys()][0]].plot(table=True)
+        else:
+            print("Plotting not currently enabled")
+        
+        
+        
     def light(self, percent=20):
         self.send_command("lights %d" % percent)
-
+        
     def calc_stime(self, scan):
         sp_scan = scan.split()
         if sp_scan[0] == "gscan":
-            time = abs(float(sp_scan[2]) - float(sp_scan[1])) * (60 / 50) * 10 * float(sp_scan[-2])
+            time = abs(float(sp_scan[2]) - float(sp_scan[1]))*(60/50)*10*float(sp_scan[-2])
         elif sp_scan[0] == "run_eems":
-            time = 15 * 60
+            time = 15*60
         elif sp_scan[0] == "cscan":
             time = float(sp_scan[4])
         elif sp_scan[0] == "ascan":
             time = float(sp_scan[4]) * float(sp_scan[5])
         elif sp_scan[0] == "cmesh":
             time = float(sp_scan[4]) * float(sp_scan[8])
-        time = time * 1.5
+        time = time *1.5
         return time
-
-    def set_recipient(self, email):
-        self.send_command("ALERT_EMAIL = sprintf(")
-
+        
     def make_macro(self, plate, coords=True):
         self.total_time = 0
         with open("/home/sgm/SpecMacros/plate.mac", 'w') as f:
@@ -585,18 +625,18 @@ class SpecClient:
                 if isinstance(s['coords'], tuple):
                     s['coords'] = [s['coords'] for i in range(0, len(s['sample']))]
                 for i, sample in enumerate(s['sample']):
-                    f.write("sample \"%s\" %d \n" % (sample, s['type']))
-                    coord = s['coords'][i]
-                    f.write("umv xp %f; umv yp %f \n" % coord)
-                    if 'scan' in s.keys():
-                        scan = s['scan']
-                    elif 'scans' in s.keys():
-                        scan = s['scans'][i]
-                    if coords:
-                        f.write("%s %f %f \n" % (scan, coord[0], coord[1]))
-                    else:
-                        f.write("%s \n" % scan)
-                    self.total_time += self.calc_stime(scan)
+                        f.write("sample \"%s\" %d \n" % (sample, s['type']))
+                        coord = s['coords'][i]
+                        f.write("umv xp %f; umv yp %f \n" % coord)
+                        if 'scan' in s.keys():
+                            scan = s['scan']
+                        elif 'scans' in s.keys():
+                            scan = s['scans'][i]
+                        if coords:
+                            f.write("%s %f %f \n" % (scan, coord[0], coord[1]))
+                        else:
+                            f.write("%s \n" % scan)
+                        self.total_time += self.calc_stime(scan)
                 if 'det' in s.keys():
                     if 'tey' in str(s['det']).lower():
                         f.write('OPTIMIZE_TEY = 0\n')
@@ -605,11 +645,11 @@ class SpecClient:
                 f.write("#-------------------------------------------------------\n")
             f.write("sendmesg(\"Your macro is now complete\")")
         print("Estimated total time for this macro is %s." % str(datetime.timedelta(seconds=self.total_time)))
-
+            
     def run_macro(self, path=None):
         if not self.total_time:
             print("You need to make a macro before you run a macro.")
-            return
+            return 
         else:
             estimate_complete = datetime.datetime.now() + datetime.timedelta(seconds=self.total_time)
             print("Estimated time of completion: %s " % estimate_complete)
